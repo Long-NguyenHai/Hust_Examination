1857779df16093271646018cbba24cee84bbd799
phil-nguyen <phil.nguyen@avepoint.com>
2023-01-26 01:34:32 +0700

    Logging

--- src/Examination/Examination.API/Controllers/V1/ExamsController.cs
+++ src/Examination/Examination.API/Controllers/V1/ExamsController.cs
@@ -12,17 +12,23 @@ namespace Examination.API.Controllers.V1
     public class ExamsController : ControllerBase
     {
         private readonly IMediator _mediator;
-
-        public ExamsController(IMediator mediator)
+        private readonly ILogger<ExamsController> _logger;
+        public ExamsController(IMediator mediator, ILogger<ExamsController> logger)
         {
             _mediator = mediator;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetExamList()
         {
+            _logger.LogInformation("BEGIN: GetExamList");
+
             var query = new GetHomeExamListQuery();
             var queryResult = await _mediator.Send(query);
+
+            _logger.LogInformation("END: GetExamList");
+
             return Ok(queryResult);
         }
 
